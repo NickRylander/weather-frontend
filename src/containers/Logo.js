49f561d88c98b16ac2f80436de7b7b7f6e9f9f14
NickRylander <nick.rylander@outlook.com>
2021-01-20 17:51:58 -0800

    cleaning up code

--- src/containers/Logo.js
+++ src/containers/Logo.js
@@ -3,8 +3,12 @@ import React from 'react';
 const Logo = () => {
     return(
         <div style={{textAlign: "center"}}>
-            <img style={{height: "100px", width:"150px", position: "relative", borderRadius: "25px"}}src="https://www.xda-developers.com/files/2019/09/Bowvie-Weather.jpg" />
-            <br/>
+            <img style={{
+                height: "100px",
+                width:"150px",
+                position: "relative",
+                borderRadius: "25px"}}
+                src="https://www.xda-developers.com/files/2019/09/Bowvie-Weather.jpg" />
         </div>
     )
 }
